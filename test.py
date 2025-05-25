728ea0b33dc9b328afe6780201465c7c60ec5db6
Dat2906041995 <121652492+29Schiller@users.noreply.github.com>
2025-05-26 02:03:45 +0700

    summary

--- test.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-# Create a Spark session
-spark = SparkSession.builder.appName("APS").master("local[*]").config("spark.driver.memory", "4g").getOrCreate()    
-
-print("✅ Spark Session created successfully!")
-# Check if Spark session is created
-if spark:
-    print("✅ Spark session is created successfully!")
